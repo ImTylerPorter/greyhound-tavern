4be6901f1efc2c812b48f013a26236a0f536179c
Tyler Porter <hello@cleverladder.com>
2024-10-29 08:25:15 -0700

    Remove supabase stuff

--- src/hooks.server.js
+++
@@ -1,9 +0,0 @@
-import { supabase } from '$lib/supabase';
-
-export async function handle({ event, resolve }) {
-  const session = await supabase.auth.getSession();
-  event.locals.session = session;
-
-  const response = await resolve(event);
-  return response;
-}
--- src/lib/session.js
+++
@@ -1,5 +0,0 @@
-// src/lib/session.js
-import { writable } from 'svelte/store';
-
-// Initialize the session store with `null` or an empty object as default
-export const session = writable(null);
